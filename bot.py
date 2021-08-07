25e567a08c5b7ff4a8fa00ed7368d0a27b6f17bf
Mathias Pilch <mathias.pilch@mailbox.org>
2021-08-07 20:38:02 +0200

    Added bot.py for testing, and .gitignore

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+
+import json
+
+with open('config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+
+TOKEN = config_data['token']
+
+description = """Description text"""
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def hello(ctx):
+    """ Says world"""
+    await ctx.send("world")
+
+@bot.command()
+async def turist(ctx):
+    """ Says Turist"""
+    await ctx.send("Turist")
+
+@bot.command()
+async def add(ctx, left : int, right : int):
+    """Adds two integers together."""
+    await ctx.send(left + right)
+
+@add.error # <- the name of the command + .error
+async def add_error(ctx, error):
+    #if isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send("Enter two integers, stupid!")
+
+@bot.command()
+async def div(ctx, numerator : float, denominator : float):
+    """Divides two floats."""
+    await ctx.send(numerator / denominator)
+
+bot.run(TOKEN)
