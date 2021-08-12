9fff476deb3d85ab727b9ea99e9d701c0b4377a0
Mathias Pilch <mathias.pilch@mailbox.org>
2021-08-12 22:30:51 +0200

    Fixed week functionality, added logging

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import discord
 from discord.ext import commands
-
 import json
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord-bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 with open('config.json', 'r') as config_file:
     config_data = json.load(config_file)
@@ -44,12 +50,15 @@ async def div(ctx, numerator: float, denominator: float):
     await ctx.send(numerator / denominator)
 
 
-emoji_numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣"]
-
 @bot.event
-async def week(message):
+async def on_message(message):
     if message.channel.name == ("test"):
+        print(message.content)
+        #emoji = discord.utils.get(client.emojis, name=':zero:')
+        emoji_numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣"]
+
         if message.content.startswith("Week"):
+            #await message.channel.send("pies are better than cakes. change my mind.")
             for emoji in emoji_numbers:
                 await message.add_reaction(emoji)
         await bot.process_commands(message)
